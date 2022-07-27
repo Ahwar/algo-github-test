8f19f2f88323cd4bef69bf7f6775c746b97332b7
ahwar <pakleo123@gmail.com>
2022-07-27 16:18:49 +0500

    feat: optimization to get parse, copy, bad command
    in this commit used list comprehension to make code more efficient to read

--- python_test.py
+++ python_test.py
@@ -2,23 +2,68 @@ import json
 import random
 
 
-def main() -> (dict, dict, dict, dict,dict, ):
+def get_parse_commands(data: dict) -> list:
+    """Get all the parse commands
+
+    returns all the parse commands
+    arguments:
+        data: the dictionary containing data
+    return:
+        parse_commands: list of parse commands
+    """
+
+    parse_commands = [
+        row for row in data if "function" in row and row["function"] == "parse"
+    ]
+    return parse_commands
+
+
+def get_copy_commands(data: dict) -> list:
+    """Get all the copy commands
+
+    returns all the copy commands
+    arguments:
+        data: the dictionary containing data
+    return:
+        copy_commands: list of copy commands
+    """
+
+    copy_commands = [
+        row for row in data if "function" in row and row["function"] == "copy"
+    ]
+    return copy_commands
+
+
+def get_bad_commands(data: dict) -> int:
+    """Get all the bad commands
+
+    returns all the bad commands
+    arguments:
+        data: the dictionary containing data
+    return:
+        bad_commands: list of bad commands
+    """
+
+    bad_commands = [
+        row for row in data if "function" in row and row["function"] == "bad value"
+    ]
+    bad_commands.append(
+        [row for row in data if "value" in row and row["value"] == "bad value"]
+    )
+    return bad_commands
+
+
+def main() -> (dict, dict, dict, dict, dict):
     # NOTE: Get all the parse commands
-    with open('data.txt', 'r') as file:
+    with open("data.txt", "r") as file:
         data = json.loads(file.read())
-    parse_commands = []
-    for row in data:
-        if 'function' in row and row['function'] == 'parse':
-            parse_commands.append(row.copy())
+    parse_commands = get_parse_commands(data=data)
     print(f"parse_commands: {parse_commands}")
 
     # NOTE: Get all the copy commands
-    with open('data.txt', 'r') as file:
+    with open("data.txt", "r") as file:
         data = json.loads(file.read())
-    copy_commands = []
-    for row in data:
-        if 'function' in row and row['function'] == 'copy':
-            copy_commands.append(row.copy())
+    copy_commands = get_copy_commands(data=data)
     print(f"copy_commands: {copy_commands}")
 
     # NOTE: Put the two lists together and say which list it came from as well as the item number for that list
@@ -37,19 +82,19 @@ def main() -> (dict, dict, dict, dict,dict, ):
         new_row['_list'] = 'copy'
         new_row['_counter'] = counter
         functional_commands.append(new_row)
-    
+
     print(f"functional_commands: {functional_commands}")
 
     # NOTE: Get random sampling of data
     random_commands = []
-    with open('data.txt', 'r') as file:
+    with open("data.txt", "r") as file:
         data = json.loads(file.read())
         random_commands = random.sample(data, 2)
     print(f"random_commands: {random_commands}")
 
     # NOTE: Write the methodology to catch bad_commands
     bad_commands = list()
-    bad_commands = [None]*3
+    bad_commands = get_bad_commands(data=data)
     return parse_commands, copy_commands, functional_commands, random_commands, bad_commands
 
 
